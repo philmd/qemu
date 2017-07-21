0a77936614f3c4460343444d997aad6d6e1e24d7
Philippe Mathieu-Daudé <f4bug@amsat.org>
2017-07-21 14:57:26 -0300

    iommu: use QEMU_IS_ALIGNED macro
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- hw/i386/intel_iommu.c
+++ hw/i386/intel_iommu.c
@@ -623,7 +623,7 @@ static inline bool vtd_iova_range_check(uint64_t iova, VTDContextEntry *ce)
      * Check if @iova is above 2^X-1, where X is the minimum of MGAW
      * in CAP_REG and AW in context-entry.
      */
-    return !(iova & ~(vtd_iova_limit(ce) - 1));
+    return QEMU_IS_ALIGNED(iova, vtd_iova_limit(ce));
 }
 
 static const uint64_t vtd_paging_entry_rsvd_field[] = {
