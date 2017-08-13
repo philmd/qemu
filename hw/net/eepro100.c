84e0c746176f2e1ee825b2800043b9874925dd29
Philippe Mathieu-Daudé <f4bug@amsat.org>
2017-08-13 13:54:56 -0300

    net/eepro100: align to DWORD (32bit)
    also use QEMU_ALIGNED()
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- hw/net/eepro100.c
+++ hw/net/eepro100.c
@@ -265,7 +265,7 @@ typedef struct {
 
     /* Data in mem is always in the byte order of the controller (le).
      * It must be dword aligned to allow direct access to 32 bit values. */
-    uint8_t mem[PCI_MEM_SIZE] __attribute__((aligned(8)));
+    uint8_t mem[PCI_MEM_SIZE] QEMU_ALIGNED(4);
 
     /* Configuration bytes. */
     uint8_t configuration[22];
