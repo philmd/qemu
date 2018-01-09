a8712844fb6531fba23ea146e91dc255b1373506
Philippe Mathieu-Daudé <f4bug@amsat.org>
2018-01-09 13:41:19 -0300

    hw/sd/ssi-sd: remove unuseful assignation
    This is now set in ssi_sd_reset()
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- hw/sd/ssi-sd.c
+++ hw/sd/ssi-sd.c
@@ -244,7 +244,6 @@ static void ssi_sd_realize(SSISlave *d, Error **errp)
     ssi_sd_state *s = FROM_SSI_SLAVE(ssi_sd_state, d);
     DriveInfo *dinfo;
 
-    s->mode = SSI_SD_CMD;
     /* FIXME use a qdev drive property instead of drive_get_next() */
     dinfo = drive_get_next(IF_SD);
     s->sd = sd_init(dinfo ? blk_by_legacy_dinfo(dinfo) : NULL, true);
