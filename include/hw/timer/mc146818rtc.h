289789428935ea193567ff207ba3cab18988488b
Philippe Mathieu-Daudé <philmd@redhat.com>
2019-02-01 22:03:37 +0100

    include rtc regs

--- include/hw/timer/mc146818rtc.h
+++ include/hw/timer/mc146818rtc.h
@@ -3,7 +3,6 @@
 
 #include "qemu/queue.h"
 #include "hw/isa/isa.h"
-#include "hw/timer/mc146818rtc_regs.h"
 
 #define TYPE_MC146818_RTC "mc146818rtc"
 #define MC146818_RTC(obj) OBJECT_CHECK(RTCState, (obj), TYPE_MC146818_RTC)
